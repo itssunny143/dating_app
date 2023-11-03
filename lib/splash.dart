128d526c7f1b195f2d6ee754242ea813a000bc23
Muhammad Saqlain <129774784+Saqlainqureshi11@users.noreply.github.com>
2023-11-03 06:27:25 +0000

    Updated

---
+++ lib/splash.dart
@@ -0,0 +1,15 @@
+import 'package:datingfest/flutter/packages/flutter/lib/material.dart';
+
+class splashscreen extends StatefulWidget {
+  const splashscreen({super.key});
+
+  @override
+  State<splashscreen> createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
